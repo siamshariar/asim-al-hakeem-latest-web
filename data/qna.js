d9b65cdaa9c5eecf457f1721d3b2537bd7f5437e
Siam Al Sahariar <siam.it.int@gmail.com>
2026-04-19 23:52:01 +0600

    Update UI for full web

--- data/qna.js
+++ data/qna.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b71b8b7a9e5ba7d94b3f03d1fb10eadafdc6d1d4bf3daf55d3a4387dd77f7b1f
-size 8222
+oid sha256:7a6cb2b049897b320141fbfd01145e5d12ec0ed21ee886a2f724b58178b2a565
+size 4608
