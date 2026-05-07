dbeff221f2caf3363f2220ac4591052a9cce52f9
Siam Al Sahariar <siam.it.int@gmail.com>
2026-05-07 23:03:10 +0600

    fix qna data

--- data/qna.js
+++ data/qna.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a9128f38813c16ea44bb3d271bbafda97ba04effd71a6fd1582d50b752c50d4
-size 8864287
+oid sha256:52eabb8fb5041880fe2c015755d9f182a878d83e7208c67e75907fcacfa8d9a2
+size 8864102
